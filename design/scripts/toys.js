e1e2dcff482ea405b1a61126de43e882bbfeb7e6
recluse <andrew@reclusedesigns.com.au>
2012-09-10 21:34:42 +1000

    start of the website

---
+++ design/scripts/toys.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	
+	
+	$('.carousel').carousel('pause');
+
+	
+	$('.tooltip_js').tooltip();
+	
+	$('.scrollto_js').click(function(e) {
+		e.preventDefault();
+		$.scrollTo($($(e.currentTarget).attr('href')), 300);
+	});
+	
+	$('.js_contact_click').click(function() {
+		$('.js_contact_toggle').slideToggle(function() {
+    // Animation complete.
+		});
+	});
+	
+	$('#slider').nivoSlider({
+		pauseTime: 4000,
+		effect: 'fade',
+		directionNavHide: false,
+	});
+	
+	$().UItoTop({ easingType: 'easeOutQuart' });
+
+	
+}); 
