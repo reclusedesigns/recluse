bdbf785c7ea1faeae72c02d4ecc726d806c1c249
recluse <andrew@reclusedesigns.com.au>
2012-10-01 22:01:12 +1000

    added the sexy 3ds

--- index.php
+++ index.php
@@ -4,14 +4,34 @@
 	
 	<div class="hero">
 		<div class="container">
-			<div class="span5">
-				<div class="intro">
-					<h2><span>Sup</span></h2>
-					<p>My name is Andrew and I design and build websites. I work for <a href="#">Squareweave</a> where we focus on making the internet a better place.</p>
+			<div class="row">
+				<div class="span5">
+					<div class="intro">
+						<h2><span>Hello</span></h2>
+						<p>My name is Andrew and I <a href="#">design and build premium websites</a> for small businesses.</p>
+					</div>
 				</div>
 			</div>
 		</div> <!-- /container -->
 	</div>
+	
+	<div class="about">
+		<div class="container">
+			<hr>
+			<div class="row">
+				<div class="span6">
+					<p>Do the carousels</p>
+				</div>
+				<div class="span6">
+					<div class="about">
+						<h2><span>The internet</span></h2>
+						<p>Don't freak out - it's not that bad. I've been doing this for a while now and I'd love to help <a href="#">make things clearer</a>.</p>
+						<p>I work as the Design Lead at <a href="#">Squareweave</a> where we focus on making the world a better place using the internet.</p>
+					</div>
+				</div>
+			</div> <!-- row -->
+		</div>
+	</div><!-- about -->
 	
 </section>
 <?php include 'footer.php'; ?>
