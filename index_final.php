229ba6562e2ac84c1aab5825c05931a665f101aa
recluse <andrew@reclusedesigns.com.au>
2012-10-10 23:13:51 +1100

    under construction page

---
+++ index_final.php
@@ -0,0 +1,86 @@
+<?php include 'header.php'; ?>
+
+<section class="home">
+	
+	<div class="hero">
+		<div class="container">
+			<div class="row">
+				<div class="span5">
+					<div class="intro">
+						<h2><span>Hello</span></h2>
+						<p>My name is Andrew and I design and build <a href="#">premium websites</a> for small businesses.</p>
+					</div>
+				</div>
+			</div>
+		</div> <!-- /container -->
+	</div>
+	
+	<!--
+<div class="testimonials">
+		<div class="container">
+			<hr>
+				<ul class="unstyled">
+					<li>"Andrew was great, he was really full of good" - Smart Dude</li>
+					<li>"Oh wow, I can't believe how good his good was" - Client</li>
+				</ul>
+			<hr>
+		</div>
+	</div>
+-->
+	
+	<div class="trust">
+		<div class="container">
+			<div class="row">
+				<div class="span8">
+					
+					<p>Recent work</p>
+					<div class="row">
+						
+						<div class="span4">
+							<a href="#"><img src="http://placekitten.com/300/200"></a>
+						</div>
+						<div class="span4">
+							<a href="#"><img src="http://placekitten.com/300/200"></a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="span4">
+					<div class="about">
+						<h2><span>The internet</span></h2>
+						<p>Don't freak out - it's not that scary. I've been doing this for a while now and I'd love to help make things clearer.</p>
+						<p>
+					</div>
+				</div>
+			</div> <!-- row -->
+		</div>
+	</div><!-- about -->
+	
+	
+	
+	<div class="experience">
+		<div class="container">
+			<div class="row">
+				
+				<div class="span6">
+					<h2><span>Skilled</span></h2>
+					<p>During the day, I work as the Design Lead at <a href="#">Squareweave</a> where we focus on making the world a better place using the internet.</p>
+					<p>From to concept work to front-end coding.
+				</div>
+				
+				<div class="span6">
+					<ul class="unstyled">
+						<li>Consultation:</li>
+						<li></li>
+						<li></li>
+						<li>Consultation</li>
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+	
+</section>
+<?php include 'footer.php'; ?>
