da3f575c4f5c6ef9c93cb19d73ec392b5789da23
recluse <andrew@reclusedesigns.com.au>
2012-10-11 22:15:21 +1100

    pausing home work, starting work page

--- index_final.php
+++ index_final.php
@@ -5,7 +5,7 @@
 	<div class="hero">
 		<div class="container">
 			<div class="row">
-				<div class="span5">
+				<div class="span4">
 					<div class="intro">
 						<h2><span>Hello</span></h2>
 						<p>My name is Andrew and I design and build <a href="#">premium websites</a> for small businesses.</p>
@@ -15,52 +15,58 @@
 		</div> <!-- /container -->
 	</div>
 	
-	<!--
-<div class="testimonials">
-		<div class="container">
-			<hr>
-				<ul class="unstyled">
-					<li>"Andrew was great, he was really full of good" - Smart Dude</li>
-					<li>"Oh wow, I can't believe how good his good was" - Client</li>
-				</ul>
-			<hr>
-		</div>
-	</div>
--->
 	
-	<div class="trust">
+	
+	<div class="internet">
 		<div class="container">
 			<div class="row">
 				<div class="span8">
-					
-					<div class="row">
-						<div class="recent_work">
-							<div class="span4">
-								<div class="fadehover">
-									<p style="z-index:200;">Lol</p>
-									<a href="#"><img src="design/images/body/circle_bw.jpg" class="bw"></a>
-									<a href="#"><img src="design/images/body/circle.jpg" class="c"></a>
-								</div>
-							</div>
-							
-							<div class="span4">
-								<div class="fadehover">
-									<a href="#"><img src="design/images/body/circle_bw.jpg" class="bw"></a>
-									<a href="#"><img src="design/images/body/circle.jpg" class="c"></a>
-								</div>
-							</div> <!-- span8 -->
-						</div>
-					</div>
+				
 				</div>
+				<div class="span4">
+					<h2><span>The internet</span></h2>
+					<p>Don't freak out - it's actually not that scary. I've been doing this for a while now and I'd love to help make things clearer.</p>
+					<p>Have a look at some of<a href="#"> my work</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	
+	<div class="branding">
+		<div class="container">
+		
+			<div class="row">
 				
 				<div class="span4">
 					<div class="about">
-						<h2><span>The internet</span></h2>
-						<p>Don't freak out - it's not that scary. I've been doing this for a while now and I'd love to help make things clearer.</p>
-						<p>Have a look at some of<a href="#"> my work</a></p>
+						<h2><span>Your brand</span></h2>
+						<p>This isn't just your logo.</p>
+						<p>I was taught that your brand is what your audience perceives you to be. Let's go create and attract your perfect audience.  <a href="#">See some work</a></p>
 						<p>
 					</div>
 				</div>
+
+					
+				<div class="recent_work">
+					<div class="span4">
+						<div class="fadehover">
+							<p style="z-index:200;">Lol</p>
+							<a href="#"><img src="design/images/body/circle_bw.jpg" class="bw"></a>
+							<a href="#"><img src="design/images/body/circle.jpg" class="c"></a>
+						</div>
+					</div>
+					
+					<div class="span4">
+						<div class="fadehover">
+							<a href="#"><img src="design/images/body/circle_bw.jpg" class="bw"></a>
+							<a href="#"><img src="design/images/body/circle.jpg" class="c"></a>
+						</div>
+					</div>
+					
+				
 			</div> <!-- row -->
 		</div>
 	</div><!-- about -->
@@ -72,6 +78,8 @@
 			<div class="row">
 				
 				<div class="span6">
+				
+					
 					<h2><span>Skilled</span></h2>
 					<p>During the day, I work as the Design Lead at <a href="#">Squareweave</a> where we focus on making the world a better place using the internet.</p>
 					<p>From to concept work to front-end coding.
